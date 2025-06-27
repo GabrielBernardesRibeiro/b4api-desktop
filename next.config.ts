e0828fe7f0d04f4ce12d3d1c1b59784523fe26c4
Gabriel Bernardes Ribeiro <gabriel.bernardes.ribeiro@hotmail.com>
2025-06-27 16:46:39 -0300

    Finish setup to create electron build and installer

---
+++ next.config.ts
@@ -0,0 +1,12 @@
+import type { NextConfig } from "next";
+
+const isStandalone = process.env.NEXT_ENV_OUTPUT_TYPE === "standalone";
+
+const nextConfig: NextConfig = {
+  output: isStandalone ? "standalone" : "export",
+  images: {
+    unoptimized: isStandalone ? false : true,
+  },
+};
+
+export default nextConfig;
