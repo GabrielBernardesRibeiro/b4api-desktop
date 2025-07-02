e11cca3332f61e511fcf5f644d3a2d9e811e704d
Gabriel Bernardes Ribeiro <gabriel.bernardes.ribeiro@hotmail.com>
2025-07-02 16:16:28 -0300

    change background color

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -36,7 +36,7 @@ export default function Home() {
   }, []);
 
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] bg-black">
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] bg-white">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <Image src={"/logo.svg"} width={400} height={400} alt="logo" />
       </main>
