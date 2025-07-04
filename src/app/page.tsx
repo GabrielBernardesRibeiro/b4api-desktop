e563549953177645fc7896fcd81e7135aeb23b48
Gabriel Bernardes Ribeiro <gabriel.bernardes.ribeiro@hotmail.com>
2025-07-04 01:31:35 -0300

    invert colors to check update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -58,12 +58,12 @@ export default function Home() {
   }, []);
 
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] bg-white">
+    <div className="bg-black grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] ">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <Image src={"/logo.svg"} width={400} height={400} alt="logo" />
       </main>
 
-      <footer className="row-start-3 flex flex-col gap-[24px] items-center justify-center text-black">
+      <footer className="row-start-3 flex flex-col gap-[24px] items-center justify-center text-white">
         <p className="text-gray-700 font-medium">B4API - new update</p>
 
         {latestVersion && latestVersion !== appVersion ? (
